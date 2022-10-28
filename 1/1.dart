a7415297bc1604384cd752a40af68aae8fff7bea
mohammed-mehdi237 <mohammed.mahdi21051a@coeng.uobaghdad.edu.iq>
2022-10-28 20:22:53 +0300

    first commit

---
+++ 1/1.dart
@@ -0,0 +1,24 @@
+/*
+write a variable in each of below value type and print its value (try to not use 'var/final/const' keyword)
+the final output may be like this
+true
+4
+1.2
+ok
+[4,3,"ok",true]
+ */
+
+void main() {
+  bool var1 = true;
+  int var2 = 4;
+  double var3 = 1.2;
+  String var4 = "ok";
+  List var5 = [4,3,"\"ok\"",true];
+  print(var1);
+  print(var2);
+  print(var3);
+  print(var4);
+  print(var5);
+  
+
+}
